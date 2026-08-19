55838fe518a84db6ac2d0f81f82125d78af47796
leooel911 <leooel911@gmail.com>
2026-08-20 00:32:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ with st.expander("📁 管理員專用：Database "):
     elif password_input != "":
         st.error("❌ 密碼錯誤，請洽C.L.F。")
 
-target_input = st.text_input("輸入 員編 或 姓名 (例如: A023300 或 台積電)", value="A018896")
+target_input = st.text_input("輸入 員編 或 姓名 (例如: A023300 或 台積電)", value="A")
 
 if st.button("立即打造個人班表圖片"):
     if not any(os.path.exists(path) for path in ROLE_FILES.values()):
