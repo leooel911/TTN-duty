b73525e65074d75996a1b46cd13c8635d3c4af19
leooel911 <leooel911@gmail.com>
2026-09-15 12:11:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -397,7 +397,7 @@ CUSTOM_CSS: str = """
 # 📧 系統管理員郵件通知與 SMTP 參數設定
 # ==========================================
 SMTP_SERVER: str = "smtp.gmail.com"        # 例如使用 Gmail 伺服器
-SMTP_PORT: int = 587                       # TLS 連接埠
+SMTP_PORT: int = 465                       # TLS 連接埠
 SENDER_EMAIL: str = "leooel911@gmail.com"     # 填入你的 Google 帳號
 SENDER_PASSWORD: str = "aoisluiqatzsmlec"        # 填入 Gmail 的「應用程式密碼」(16碼)
 ADMIN_RECEIVE_EMAIL: str = "leooel911@gmail.com" # 收件人信箱
